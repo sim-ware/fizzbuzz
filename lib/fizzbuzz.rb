f85f30b29e2f516f5ae4ee967fa315cff52fd707
Samir Gossain <samir.gossain@gmail.com>
2016-12-20 14:14:16 +0000

    Returns Fizzbuzz for mutliples of 3 & 5, number itself for non multiples of 3 & 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,15 @@
 def fizzbuzz(number)
    # if multiple of 3'fizz'
-   if number%3 == 0
-     return 'fizz'
+   if (number%3 == 0) && (number%5 == 0)
+     return 'fizzbuzz'
    # if multiple of 5'buzz'
-   elsif number%5 == 0
+ elsif number%3 == 0
+     return 'fizz'
+   # if multiple of 3 & 5 'fizzbuzz'
+ elsif number%5 == 0
      return 'buzz'
+   # if not multiple of 3 or 5, return the number
+else
+     return "#{number}".to_i
    end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,10 @@ describe 'fizzbuzz' do
   it 'return "buzz" when passed multiple of 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed multiple of 3 & 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns the number if fed a non-multiple of 3 or 5' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
