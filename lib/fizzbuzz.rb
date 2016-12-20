c0cc39e6fc8793e607c5c89cefc13ff491daff19
Samir Gossain <samir.gossain@gmail.com>
2016-12-20 14:26:16 +0000

    Refactors Code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,14 @@
 def fizzbuzz(number)
-   # if multiple of 3 & 5 'fizzbuzz'
-   if (number%3 == 0) && (number%5 == 0)
-     return 'fizzbuzz'
-   # if multiple of 3 'fizz'
- elsif number%3 == 0
-     return 'fizz'
-   # if multiple of 5 'buzz'
- elsif number%5 == 0
-     return 'buzz'
-   # if not multiple of 3 or 5, return the number
-else
-     return number
-   end
+  # if multiple of 3 & 5 'fizzbuzz'
+  if (number%3 == 0) && (number%5 == 0)
+    return 'fizzbuzz'
+    # if multiple of 3 'fizz'
+  elsif number%3 == 0
+    return 'fizz'
+    # if multiple of 5 'buzz'
+  elsif number%5 == 0
+    return 'buzz'
+    # if not multiple of 3 or 5, return the number
+  end
+    return number
 end
